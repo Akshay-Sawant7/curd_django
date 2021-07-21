de8f095c2aa738679342847b3c43a7c350cbe65c
Akshay <akshaybsawant22@gmail.com>
2021-07-21 21:09:12 +0530

    procfile

--- projectcurd/curdApp/models.py
+++ projectcurd/curdApp/models.py
@@ -4,6 +4,6 @@ class User(models.Model):
     name=models.CharField(max_length=20)
     email=models.EmailField(max_length=30)
     password=models.CharField(max_length=20)
-    address = models.CharField(max_length=20)
+    address=models.CharField(max_length=20)
 
 
